75f032c88c21f4d023bf1903d7053ceaa66bbb47
KangShinHyeon <rkdrpgml@naver.com>
2021-05-07 23:07:53 +0900

    initial_repository

---
+++ 1_print.py
@@ -0,0 +1,56 @@
+# print 구문의 이해
+
+# 기본출력
+print('Hello python!')
+print("Hello python!")
+print("""Hello python!""")
+print('''Hello python!''')
+
+print() # 아무것도 출력하지 않음 -> enter효과
+
+# separator
+print('T', 'E', 'S', 'T')
+print('T', 'E', 'S', 'T', sep='')
+print('2019', '02', '19', sep='-')
+print('rkdrpgml','naver.com',sep='@')
+
+# end
+print('Welcom To', end=' ')
+print('the black parade', end=' ')
+print('piano notes')
+print()
+
+# format [], {}, ()
+print('{} and {}'.format('You', 'Me'))
+print('{0} and {1} and {0}'.format('You', 'Me'))
+print("{a} are {b}".format(a='You', b='Me'))
+
+print("%s's favorite number is %d" % ('Eunki', 7))
+
+print("Test1 : %5d, price : %4.2f" % (776, 6534.123))
+print("Test1 : {0: 5d}, price :{1: 4.2f}".format(776, 6543.123))
+print("Test1 : {a: 5d}, price :{b: 4.2f}".format(a=776, b=6543.123))
+print()
+
+# Escape 코드
+"""
+\n : 개행
+\t : 탭
+\\ : 문자
+\' : 문자
+\" : 문자
+\r : 캐리지 리턴
+\f : 폼 피드
+\a : 벨 소리
+\b : 백 스페이스
+\000 : 널 문자
+"""
+
+print("'you'")
+print('"you"')
+
+print('\'you\'') # \다음 문자는 문자 그대로 출력
+print('\\you\\') 
+
+print('\\you\\\n\n') # \n = enter
+print('\t\tyou') # \t = 들여쓰기
---
+++ 2_basic_coding.py
@@ -0,0 +1,41 @@
+# 파이썬 기초 코딩
+# 몸풀기 코딩 실습
+
+# import this # 파이썬의 철학 및 장점들 출력됨
+
+# python3의 입력 및 출력 기본 인코딩
+import sys
+print(sys.stdin.encoding)
+print(sys.stdout.encoding)
+# utf_8임을 알 수 있음
+
+# 출력문
+print('My name is shinhyeon!')
+
+# 변수 선언
+name = 'shinhyeon'
+
+# 조건문
+if name == 'shinhyeon':
+    print('correct')
+
+# 반복문
+for i in range(1,10):
+    for j in range(1,10):
+        print('%d * %d = ' % (i,j), i*j)
+
+# 함수선언
+def A():
+    print('안녕')
+
+A()
+
+# 클래스
+class Cookie:
+    pass
+
+# 객체 생성
+cookie = Cookie()
+
+print(id(cookie))
+print(dir(cookie))
