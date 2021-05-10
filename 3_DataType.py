08dc1b48be54c0c24b8368da2f59cd83a6211076
KangShinHyeon <rkdrpgml@naver.com>
2021-05-10 23:55:10 +0900

    post_DataType_numbers

--- 3_DataType.py
+++ 3_DataType.py
@@ -31,6 +31,7 @@ print(type(v_float))
 print(type(v_int))
 print(type(v_dict))
 print(type(v_list))
+print(type(v_tuple))
 print(type(v_set))
 
 # python 숫자형 및 연산자
@@ -56,6 +57,34 @@ f4 = 10.
 print(i1 * i2)
 print(big_int1 * big_int2)
 print(f1 ** f2)
+
 print(f3 + i2) # 정수 + 실수 => 실수가 껴있으면 실수로 출력
 result = f3 + i2
 print(result, type(result))
+
+a = 5.
+b = 4
+print(type(a), type(b))
+
+result2 = a+b
+print(result2)
+
+# 형변환
+# int, float, complex(복소수)
+print(int(3.0))
+print(float(3))
+print(complex(3))
+print(int(True))
+print(int(False))
+print(int('3'))
+print(complex(False))
+
+# 수치 연산 함수
+print(abs(-7)) # 절댓값
+n, m = 10, 20
+n, m = divmod(100,8) # 100을 8로 나눈 몫을 n에, 나머지를 m에 대입
+print(n,m)
+
+import math
+print(math.ceil(5.1)) # 5.1보다 큰 정수중 가장 작은 정수
+print(math.floor(3.874)) # 3.874보다 작은 정수중 가장 큰 정수
