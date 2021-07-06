eab9e8f34fb42caadc4696173d662bacf1e3dcad
shin hyeon Kang <rkdrpgml@naver.com>
2021-07-06 15:28:13 +0900

    add datatype

--- 3_DataType.py
+++ 3_DataType.py
@@ -151,8 +151,7 @@ print(a[0:4:2]) # 4까지 2개씩
 print(a[1:-2])
 print(a[::-1]) # -1부터 전부
 
-# 리스트
-
+# ----------------------------------------------------------------------------------------
 # 리스트 : 순서 있음, 중복 허용, 수정 & 삭제 가능
 a = []
 b = list()
@@ -183,7 +182,7 @@ print(c)
 c[1] = ['a','b','c']
 print(c)
 
-# 삭제
+# 삭제 (index 기준)
 del c[1]
 print(c)
 del c[-1]
@@ -195,11 +194,166 @@ print(y)
 y.append(6)
 print(y)
 
+# sort : 정렬
 y.sort()
 print(y)
 
+# reverse
 y.reverse()
 print(y)
 
+# insert
 y.insert(2, 7) # 2번 index에 7삽입
 print(y)
+
+# 삭제 함수 (원소값 기준)
+y.remove(2)
+print(y)
+
+# pop : 마지막 원소를 꺼냄 (LIFO)
+y.pop()
+print(y)
+
+# extend : 끝에 연장
+tmp = [88,77]
+y.extend(tmp)
+print(y)
+
+y.append(tmp) # extend는 리스트 내 원소가 들어가고 append는 리스트 자체가 들어감
+print(y)
+
+# ----------------------------------------------------------------------------------------
+# 튜플 : 순서 있음, 중복 허용, 수정 & 삭제 불가능
+# 중요한 계좌번호, 개인정보 등 수정될 경우 오류를 일으키는 경우에 사용
+
+a = ()
+b = (1,)
+c = (1, 2, 3, 4)
+
+# del c[2] -> TypeError: 'tuple' object doesn't support item deletion
+
+d = (10, 100, ('a','b','c'))
+
+print()
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2:])
+print(d[2][0:2])
+
+print(c+d)
+print(c*3)
+
+print()
+
+# 함수
+z = (5, 2, 1, 3, 4, 2)
+
+print(z)
+print(3 in z)
+print(z.index(1))
+print(z.count(2))
+
+# ----------------------------------------------------------------------------------------
+# 딕셔너리 (Dictionary)
+# : 순서X, 중복X, 수정O, 삭제O
+
+a = {'name':'Kang', 'phone':'010-0000-0000', 'birth': 980823}
+b = {0:'hello', 1:'bye'}
+c = {'arr':[1, 2, 3, 4, 5]}
+
+
+# 출력
+# 1.직접접근
+print(a['name'])
+# 없는 key로 접근할 경우 error
+
+# 2.get
+print(a.get('name'))
+# 없는 key로 접근할 경우 None 출력
+
+print(c['arr'][1:3])
+print()
+
+# 추가
+a['address'] = 'Seoul'
+print(a)
+
+a['rank_list'] = [1,2,3]
+a['rank_tuple'] = (1,2,3)
+print(a)
+print()
+
+# keys
+print(a.keys())
+print(list(a.keys()))
+print()
+
+# values
+print(a.values())
+print(list(a.values()))
+print()
+
+# items
+print(a.items())
+print(list(a.items())) # list안의 tuple 형태로 출력
+print()
+
+
+# ----------------------------------------------------------------------------------------
+# 집합 (Sets)
+# : 순서X, 중복X
+
+a = set()
+b = set([1, 2, 3, 4])
+c = set([1, 4, 5, 6, 6])
+
+print(c) # 중복을 허용하지 않기 때문에 6은 한번만 출력
+print()
+
+# 집합은 보통 형 변환을 사용하여 인덱싱, 슬라이싱을 함
+print(b)
+
+t = tuple(b)
+print(t)
+
+l = list(b)
+print(l)
+print()
+
+
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+
+# 교집합
+# 1. intersection
+print(s1.intersection(s2))
+# 2. &
+print(s1 & s2)
+print()
+
+# 합집합 
+# (집합 자체가 중복을 허용하지 않기 때문에 같은 부분은 한번만 포함됨)
+# 1. union
+print(s1.union(s2))
+# 2. |
+print(s1 | s2)
+print()
+
+# 차집합
+# 1. difference
+print(s1.difference(s2))
+# 2. -
+print(s1 - s2)
+print()
+
+
+# 추가, 제거
+s3 = set([7, 8, 10, 15])
+print(s3)
+s3.add(18)
+print(s3)
+s3.remove(10)
+print(s3)
